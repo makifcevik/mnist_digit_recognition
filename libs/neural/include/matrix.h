4eaaeb3e5cca7864fd38b1e66db896c6f6412d4a
makifcevik <makif.cevik02@gmail.com>
2025-10-18 23:37:17 +0300

    Fix matrixs interface issues.

--- libs/neural/include/matrix.h
+++ libs/neural/include/matrix.h
@@ -20,18 +20,18 @@ class Matrix {
   ~Matrix() = default;
 
   // Copy
-  Matrix(const Matrix& other);
-  Matrix& operator=(const Matrix& other);
+  Matrix(const Matrix& other) = default;
+  Matrix& operator=(const Matrix& other) = default;
 
   // Move
-  Matrix(Matrix&& other) noexcept;
-  Matrix& operator=(Matrix&& other) noexcept;
+  Matrix(Matrix&& other) noexcept = default;
+  Matrix& operator=(Matrix&& other) noexcept = default;
 
   // Static Random Matrix Generator
-  static Matrix& Random(size_t rows, size_t cols, T min = T(0), T max = T(1),
+  static Matrix Random(size_t rows, size_t cols, T min = T(0), T max = T(1),
                         unsigned int seed = 0);
 
-  Matrix& GetTranspose() const;
+  Matrix GetTranspose() const;
 
   // Element Access
   T& operator()(size_t row, size_t col);
@@ -45,24 +45,21 @@ class Matrix {
   Matrix operator+(const Matrix& other) const;
   Matrix operator-(const Matrix& other) const;
 
-  Matrix operator*=(const Matrix& other);
-  Matrix operator/=(const Matrix& other);
-  Matrix operator+=(const Matrix& other);
-  Matrix operator-=(const Matrix& other);
+  Matrix& operator*=(const Matrix& other);
+  Matrix& operator/=(const Matrix& other);
+  Matrix& operator+=(const Matrix& other);
+  Matrix& operator-=(const Matrix& other);
 
   // Scalar Operations
-  Matrix operator*(T scalar) const;
-  Matrix operator/(T scalar) const;
-  Matrix operator+(T scalar) const;
-  Matrix operator-(T scalar) const;
-
-  Matrix operator*=(T scalar);
-  Matrix operator/=(T scalar);
-  Matrix operator+=(T scalar);
-  Matrix operator-=(T scalar);
+  Matrix& operator*=(T scalar);
+  Matrix& operator/=(T scalar);
+  Matrix& operator+=(T scalar);
+  Matrix& operator-=(T scalar);
 
   // Utility Functions
-  std::vector<T>& ToVector() const noexcept;
+  const std::vector<T>& ToVector() const noexcept;
+  std::vector<T>& ToVector() noexcept;
+
   size_t Rows() const noexcept;
   size_t Cols() const noexcept;
   //void Print() const;
@@ -72,5 +69,27 @@ class Matrix {
   size_t rows_;
   size_t cols_;
 };
+
+template <Numeric T>
+inline Matrix<T> operator*(Matrix<T> lhs, T rhs);
+template <Numeric T>
+inline Matrix<T> operator*(T lhs, Matrix<T> rhs);
+
+template <Numeric T>
+inline Matrix<T> operator/(Matrix<T> lhs, T rhs);
+template <Numeric T>
+inline Matrix<T> operator/(T lhs, Matrix<T> rhs);
+
+template <Numeric T>
+inline Matrix<T> operator+(Matrix<T> lhs, T rhs);
+template <Numeric T>
+inline Matrix<T> operator+(T lhs, Matrix<T> rhs);
+
+template <Numeric T>
+inline Matrix<T> operator-(Matrix<T> lhs, T rhs);
+template <Numeric T>
+inline Matrix<T> operator-(T lhs, Matrix<T> rhs);
+
+#include "matrix.inl"
 
 #endif  // MNIST_DIGIT_RECOGNITION_LIBS_NEURAL_MATRIX_H
