d4f47b61adffce6218f1a14faa81acc8118b4a90
makifcevik <makif.cevik02@gmail.com>
2025-10-23 09:24:18 +0300

    Implement logging & assertion to mnist_loader using Abseil.

--- libs/mnist_loader/src/mnist_loader.cc
+++ libs/mnist_loader/src/mnist_loader.cc
@@ -2,6 +2,9 @@
 
 #include "mnist_loader.h"
 
+#include <absl/log/check.h>
+#include <absl/log/log.h>
+
 MNISTLoader::MNISTLoader() {
   SetSystemEndianness();
 }
@@ -25,45 +28,44 @@ int32_t MNISTLoader::ReadInt32(std::ifstream& file) const {
 
 std::vector<uint8_t> MNISTLoader::ReadImages(const std::string& path) const {
   std::ifstream file(path, std::ios::binary);
-  if (!file.is_open()) {
-    // Handle error: could not open file
-  }
+  CHECK(!file.is_open()) << "Could not open file: " << path;
+  LOG(INFO) << "Reading images from: " << path;
 
   // ReadInt32() already checks for endianness and reverses if necessary
-  if (ReadInt32(file) != kImageMagicNumber) {
-    // Handle error: invalid magic number
-  }
+  CHECK(ReadInt32(file) != kImageMagicNumber)
+      << "Invalid magic number in image file: " << path;
 
   const int32_t num_images = ReadInt32(file);
   const int32_t num_rows = ReadInt32(file);
   const int32_t num_cols = ReadInt32(file);
 
+  CHECK(num_images <= 0) << "Invalid number of images in file: " << path;
+
   // MNIST images are always 28x28 pixels
-  if (num_rows != 28 || num_cols != 28) {
-    // Handle error: unexpected image dimensions
-  }
+  CHECK(num_rows != 28 || num_cols != 28)
+      << "Unexpected image dimensions in file: " << path;
 
   // Read image data
   std::vector<uint8_t> images(num_images * num_rows * num_cols);
   file.read(reinterpret_cast<char*>(images.data()), images.size());
+  LOG(INFO) << "Completed reading images from: " << path;
   return images;
 }
 
 std::vector<uint8_t> MNISTLoader::ReadLabels(const std::string& path) const {
   std::ifstream file(path, std::ios::binary);
-  if (!file.is_open()) {
-    // Handle error: could not open file
-  }
+  CHECK(!file.is_open()) << "Could not open file: " << path;
+  LOG(INFO) << "Reading labels from: " << path;
 
   // ReadInt32() already checks for endianness and reverses if necessary
-  if (ReadInt32(file) != kLabelMagicNumber) {
-    // Handle error: invalid magic number
-  }
+  CHECK(ReadInt32(file) != kLabelMagicNumber)
+      << "Invalid magic number in label file: " << path;
 
   // Read label data
   const int32_t num_labels = ReadInt32(file);
   std::vector<uint8_t> labels(num_labels);
   file.read(reinterpret_cast<char*>(labels.data()), labels.size());
+  LOG(INFO) << "Completed reading labels from: " << path;
   return labels;
 }
 
@@ -72,6 +74,7 @@ Dataset MNISTLoader::Load(const std::string& image_file_path,
   Dataset dataset;
   dataset.data = ReadImages(image_file_path);
   dataset.labels = ReadLabels(label_file_path);
+  LOG(INFO) << "MNIST dataset loaded successfully.";
   return dataset;
 }
 
