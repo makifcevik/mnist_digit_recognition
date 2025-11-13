79821ae594830bae1e3364dfdf761b78ff424426
makifcevik <makif.cevik02@gmail.com>
2025-11-13 08:02:20 +0300

    Update experiment to utilize batch-based training.

--- src/main.cc
+++ src/main.cc
@@ -11,11 +11,19 @@
 #include "mnist_loader.h"
 #include "neural_network.h"
 
+static void RunExperiment();
+
+int main() {
+  RunExperiment();
+  return 0;
+}
+
 static void RunExperiment() {
   using Fp = float;
   const size_t kNumClasses = 10;
   const float kNormalizationFactor = 1.0f / 255.0f;
-  const int kNumEpochs = 1;
+  const uint32_t kNumEpochs = 5;
+  const uint32_t kBatchSize = 64;
 
   MNISTLoader loader;
 
@@ -35,25 +43,30 @@ static void RunExperiment() {
 
   // Create neural network
   NeuralNetwork<Fp> network;
-  network.AddLayer(std::make_unique<LinearLayer<Fp>>(784, 128, 0.01f, 42));
-  network.AddLayer(std::make_unique<LinearLayer<Fp>>(128, 10, 0.01f, 43));
+  network.AddLayer(std::make_unique<LinearLayer<Fp>>(784, 10, 0.001f, 42));
+  // network.AddLayer(std::make_unique<LinearLayer<Fp>>(128, 10, 0.01f, 43));
+
+  float initial_accuracy =
+      network.EvaluateAccuracy(x_train, y_train);
+  std::cout << "Initial Training Accuracy: " << initial_accuracy * 100.0f << "%"
+            << std::endl;
 
   // Train the network
   std::cout << "Training started..." << std::endl;
   std::chrono::steady_clock::time_point start_time =
       std::chrono::steady_clock::now();
-  network.Train(x_train, y_train, kNumEpochs);
+  network.Train(x_train, y_train, kNumEpochs, kBatchSize);
   std::chrono::steady_clock::time_point end_time =
       std::chrono::steady_clock::now();
   std::cout << "Training completed." << std::endl;
   std::cout << "Time taken for " << kNumEpochs << " epoch(s): "
-            << std::chrono::duration_cast<std::chrono::seconds>(
-                   end_time - start_time)
+            << std::chrono::duration_cast<std::chrono::seconds>(end_time -
+                                                                start_time)
                    .count()
             << " seconds." << std::endl;
-}
 
-int main() {
-  RunExperiment();
-  return 0;
+  // Evaluate accuracy on training data
+  float accuracy =
+      network.EvaluateAccuracy(x_train, y_train);
+  std::cout << "Training Accuracy: " << accuracy * 100.0f << "%" << std::endl;
 }
