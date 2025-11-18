8a20ad2a4433231199153c33674dd4cb0c144402
makifcevik <makif.cevik02@gmail.com>
2025-11-18 19:44:17 +0300

    Include test data in the experiment. Make the network larger for better results.

--- src/main.cc
+++ src/main.cc
@@ -2,15 +2,15 @@
 
 #include <cstdint>
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 
 #include "linear_layer.h"
-#include "relu_layer.h"
 #include "loss.h"
 #include "matrix.h"
 #include "mnist_loader.h"
 #include "neural_network.h"
+#include "relu_layer.h"
 
 static void RunExperiment();
 
@@ -23,14 +23,16 @@ static void RunExperiment() {
   using Fp = float;
   const size_t kNumClasses = 10;
   const float kNormalizationFactor = 1.0f / 255.0f;
-  const uint32_t kNumEpochs = 3;
-  const uint32_t kBatchSize = 32;
+  const uint32_t kNumEpochs = 30;
+  const uint32_t kBatchSize = 24;
 
   MNISTLoader loader;
 
   // Load MNIST data
   auto [raw_train_img_vec, raw_train_label_vec] = loader.Load(
       "data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte");
+  auto [raw_test_img_vec, raw_test_label_vec] =
+      loader.Load("data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte");
 
   // Convert dataset to Matrix format
   Matrix<uint8_t> raw_train_images(raw_train_img_vec,
@@ -38,26 +40,42 @@ static void RunExperiment() {
   Matrix<uint8_t> raw_train_labels(raw_train_label_vec,
                                    raw_train_label_vec.size(), 1);
 
+  Matrix<uint8_t> raw_test_images(raw_test_img_vec,
+                                  raw_test_img_vec.size() / 784, 784);
+  Matrix<uint8_t> raw_test_labels(raw_test_label_vec, raw_test_label_vec.size(),
+                                  1);
+
   Matrix<Fp> x_train = raw_train_images.ToFloat(kNormalizationFactor);
   Matrix<Fp> y_train =
       Matrix<Fp>::OneHotEncode(raw_train_labels.ToFloat(), kNumClasses);
 
+  Matrix<Fp> x_test = raw_test_images.ToFloat(kNormalizationFactor);
+  Matrix<Fp> y_test =
+      Matrix<Fp>::OneHotEncode(raw_test_labels.ToFloat(), kNumClasses);
+
   // Create neural network
   NeuralNetwork<Fp> network;
-  network.AddLayer(std::make_unique<LinearLayer<Fp>>(784, 64, 0.01f, 42));
+  network.AddLayer(std::make_unique<LinearLayer<Fp>>(784, 256, 0.01f, 42));
   network.AddLayer(std::make_unique<ReLULayer<Fp>>());
-  network.AddLayer(std::make_unique<LinearLayer<Fp>>(64, 10, 0.01f, 43));
+  network.AddLayer(std::make_unique<LinearLayer<Fp>>(256, 256, 0.01f, 43));
+  network.AddLayer(std::make_unique<ReLULayer<Fp>>());
+  network.AddLayer(std::make_unique<LinearLayer<Fp>>(256, 10, 0.01f, 44));
 
-  float initial_accuracy =
-      network.EvaluateAccuracy(x_train, y_train);
-  std::cout << "Initial Training Accuracy: " << initial_accuracy * 100.0f << "%"
-            << std::endl;
+  float initial_training_accuracy = network.EvaluateAccuracy(x_train, y_train);
+  std::cout << "Initial Training Accuracy: "
+            << initial_training_accuracy * 100.0f << "%" << std::endl;
+
+  float initial_testing_accuracy = network.EvaluateAccuracy(x_test, y_test);
+  std::cout << "Initial Testing Accuracy: " << initial_testing_accuracy * 100.0f
+            << "%" << std::endl;
 
   // Train the network
   std::cout << "Training started..." << std::endl;
   std::chrono::steady_clock::time_point start_time =
       std::chrono::steady_clock::now();
-  network.Train(x_train, y_train, kNumEpochs, kBatchSize);
+
+  network.Train(x_train, y_train, x_test, y_test, kNumEpochs, kBatchSize);
+  
   std::chrono::steady_clock::time_point end_time =
       std::chrono::steady_clock::now();
   std::cout << "Training completed." << std::endl;
@@ -66,9 +84,4 @@ static void RunExperiment() {
                                                                 start_time)
                    .count()
             << " seconds." << std::endl;
-
-  // Evaluate accuracy on training data
-  float accuracy =
-      network.EvaluateAccuracy(x_train, y_train);
-  std::cout << "Training Accuracy: " << accuracy * 100.0f << "%" << std::endl;
 }
