e1094d554c11773e77a5f36964ff047de0915310
makifcevik <makif.cevik02@gmail.com>
2025-12-07 22:07:10 +0300

    Add `layer_type.h`.

---
+++ libs/neural/include/layer_type.h
@@ -0,0 +1,15 @@
+// MIT License
+
+#ifndef MNIST_DIGIT_RECOGNITION_LIBS_NEURAL_NEURAL_LAYER_TYPE_H_
+#define MNIST_DIGIT_RECOGNITION_LIBS_NEURAL_NEURAL_LAYER_TYPE_H_
+
+#include <cstdint>
+
+// Provides layer types for neural network
+enum class LayerType : uint32_t {
+  kUnknown = 0,
+  kLinear = 1,
+  kRelu = 2
+};
+
+#endif  // MNIST_DIGIT_RECOGNITION_LIBS_NEURAL_NEURAL_LAYER_TYPE_H_
