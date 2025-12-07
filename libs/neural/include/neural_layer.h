8da47f7ff97c2d1dd958d9b7752d495f643bea93
makifcevik <makif.cevik02@gmail.com>
2025-12-07 22:11:54 +0300

    Add `Type()` member function and implement `Serializable` interface.

--- libs/neural/include/neural_layer.h
+++ libs/neural/include/neural_layer.h
@@ -6,17 +6,20 @@
 #include <concepts>
 
 #include "matrix.h"
+#include "serializable.h"
+#include "layer_type.h"
 
 // Interface for neural network layers.
 // Each layer must implement Forward, Backward, and UpdateWeights methods.
 // Templated on floating-point type Fp for numerical computations.
 template <std::floating_point Fp>
-class NeuralLayer {
+class NeuralLayer : public Serializable {
  public:
   using MatrixType = Matrix<Fp>;
 
   virtual ~NeuralLayer() = default;
 
+  virtual LayerType Type() const = 0;
   virtual MatrixType Forward(const MatrixType& input) = 0;
   virtual MatrixType Backward(const MatrixType& grad_output) = 0;
   // Update the layer's weights based on the internal learning rate and gradients.
