7bf85b9d52c68f4ff3a1c7392bddb85515cf1474
makifcevik <makif.cevik02@gmail.com>
2025-12-09 08:37:38 +0300

    Add `Serialize()` function.

--- libs/neural/include/neural_network.h
+++ libs/neural/include/neural_network.h
@@ -7,8 +7,11 @@
 #include <memory>
 #include <vector>
 
+#include <absl/status/status.h>
+
 #include "matrix.h"
 #include "neural_layer.h"
+#include "layer_type.h"
 
 // Network class managing multiple neural layers (NeuralLayer<Fp> instances).
 template <std::floating_point Fp>
@@ -16,6 +19,7 @@ class NeuralNetwork {
  public:
   using MatType = typename NeuralLayer<Fp>::MatrixType;
   using NetworkLayer = NeuralLayer<Fp>;
+  using Layers = std::vector<std::unique_ptr<NeuralLayer<Fp>>>;
 
   // Default constructor and destructor
   NeuralNetwork() = default;
@@ -37,6 +41,15 @@ class NeuralNetwork {
              const MatType& raw_test_data, const MatType& raw_test_labels,
              uint32_t epochs, uint32_t batch_size);
   float EvaluateAccuracy(const MatType& data, const MatType& labels);
+
+  // Serialization
+  // NOTE: This method is intended to be used by the `ModelSerializer` class.
+  // It does not write the file header (Magic Number). To save a fully valid
+  // model file, use `ModelSerializer::Save`.
+  //
+  // To load a network, use `ModelSerializer::Load`, as this class cannot
+  // self-deserialize due to layer factory dependencies.
+  absl::Status Serialize(std::ostream& out) const;
 
   const Layers& GetLayers() const;
   void Clear();
